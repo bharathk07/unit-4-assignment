78a3be1014c30e2d3d69b2bbfc7df61e6abde881
bharathk07 <bharathkswamy07@gmail.com>
2022-03-07 20:12:48 +0530

    cal

---
+++ calculator/main.js
@@ -0,0 +1,11 @@
+const add=require('./add')
+console.log(`Addition is = ${add(5,4)}`)
+
+const add1=require('./divide')
+console.log(`Division is = ${add1(5,4)}`)
+
+const multi=require('./multiply')
+console.log(`Multiplication is = ${multi(5,4)}`)
+
+const subtract=require('./subtract')
+console.log(`Subtraction is = ${subtract(5,4)}`)
